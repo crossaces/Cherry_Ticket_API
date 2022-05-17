096561e41e13303d3063fc3e13047c7f9fc8d094
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-05-17 12:39:49 +0700

    update

--- app/Http/Controllers/Api/KotaController.php
+++ app/Http/Controllers/Api/KotaController.php
@@ -112,7 +112,7 @@ class KotaController extends Controller
 
         $updateData = $request->all();
         $validate = Validator::make($updateData, [
-            "nama_kota" => "required",
+            "nama_kota" => "required|unique:kota,". $Kota->NAMA_KOTA,
             "gambar_kota" => "required|image|mimes:jpeg,png,jpg|max:1048",
         ]);
 
