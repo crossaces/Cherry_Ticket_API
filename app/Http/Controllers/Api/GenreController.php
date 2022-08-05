dbb3e57a62e31afe68bb9126ef82c0e2e6bdd3f3
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-08-06 06:19:14 +0700

    update

--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -62,7 +62,7 @@ class GenreController extends Controller
 
     public function getAllMobile()
     {
-        $Genre =  Genre::has('event')->get();
+        $Genre =  Genre::has('event')->where("EVENT_TAB", "=", "Active")->get();
 
         if (!is_null($Genre)) {
             return response(
