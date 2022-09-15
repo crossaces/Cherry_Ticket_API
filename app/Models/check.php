75f406ff69b9708123da18d487ecdc0b930a5776
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-09-16 03:51:50 +0700

    update

--- app/Models/check.php
+++ app/Models/check.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class check extends Model
+class Check extends Model
 {
     use HasFactory;
     protected $primaryKey = "ID_CHECK";
