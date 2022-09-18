10b29a4cdb59746901e8c0e4d9c921145000b484
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-09-19 05:42:32 +0700

    update

--- app/Http/Controllers/Api/PendaftaranPesertaController.php
+++ app/Http/Controllers/Api/PendaftaranPesertaController.php
@@ -78,8 +78,7 @@ class PendaftaranPesertaController extends Controller
 
     public function getDataPendaftaranEvent($id)
     { 
-        $PendaftaranPeserta = PendaftaranPeserta::with('event.tiket','peserta',
-        'check','order.tiket')->where("ID_EVENT", "=", $id)->orderBy('ID_PENDAFTARAN', 'DESC')->get();    
+        $PendaftaranPeserta = PendaftaranPeserta::with('event.jenisacara','event.genre','event.kota','event.tiket','peserta','order.tiket')->where("ID_EVENT", "=", $id)->orderBy('ID_PENDAFTARAN', 'DESC')->get();    
         if (!is_null($PendaftaranPeserta)) {
             return response(
                 [
