d2dcd8da10c315143403aa978bb110d623d980ec
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-09-21 04:40:07 +0700

    update

--- app/Http/Controllers/Api/EvaluasiController.php
+++ app/Http/Controllers/Api/EvaluasiController.php
@@ -143,7 +143,7 @@ class EvaluasiController extends Controller
             $i++;            
             foreach($Evaluasi as $e ){
                 $j=0;
-                $e->DATA_JAWABAN = $e->DATA_JAWABAN;
+                $e->DATA_JAWABAN = json_decode($e->DATA_JAWABAN);
                 $f->DATA_JAWABAN = $e->DATA_JAWABAN;
                 // foreach($e->DATA_JAWABAN as $a ){
                 //      $j++;
