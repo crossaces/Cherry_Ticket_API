673731adbf02a8781f1dbeb931703925033c22b5
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-09-21 07:05:40 +0700

    update

--- app/Http/Controllers/Api/EvaluasiController.php
+++ app/Http/Controllers/Api/EvaluasiController.php
@@ -141,9 +141,8 @@ class EvaluasiController extends Controller
         $i=0;
         foreach($Form->DATA_PERTANYAAN as $f ){
             $i++;      
-            $temps=$f->OPTIONS;
             if($f->TYPE != "Text"){
-                $f->DATA_JAWABAN=$temps;
+                $f->DATA_JAWABAN=$f->OPTIONS;
                 foreach($f->DATA_JAWABAN as $o ){                
                         $o->JUMLAH= 0;                                               
                 }
