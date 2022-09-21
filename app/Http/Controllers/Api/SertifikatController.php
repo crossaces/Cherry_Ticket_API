2dcd72daadedd7ce7c6121b885a2f60d0e4c1ac1
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-09-22 05:34:02 +0700

    Update

--- app/Http/Controllers/Api/SertifikatController.php
+++ app/Http/Controllers/Api/SertifikatController.php
@@ -80,7 +80,7 @@ class SertifikatController extends Controller
 
         // save the image
         $temp=imagejpeg($image, 'Test', $quality = 100);
-        return Response::download('public_path()'.'Test');   
+        return Response::download(public_path().'Test');   
     }
 
 
