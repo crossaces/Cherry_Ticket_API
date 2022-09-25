416ff2a4b7674c5e2cd7535452bc3f9912523af1
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-09-26 06:08:36 +0700

    update

--- app/Exports/LaporanExport.php
+++ app/Exports/LaporanExport.php
@@ -8,13 +8,25 @@ class LaporanExport implements FromView
 {
     
 
-    public function __construct($data){
-        $this->$data = $data;
-    }
+    // public function __construct($data){
+    //     $this->$data = $data;
+    // }
 
+    // public function view(): View
+    // {
+    //     return view('laporan',[
+    //         'data'=>$this->data
+    //     ]);
+    // }
+
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
     public function view(): View
     {
-        return view('laporan',[
+        return view('productsExport',[
             'data'=>$this->data
         ]);
     }
