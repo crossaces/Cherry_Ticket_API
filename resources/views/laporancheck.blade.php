cf6223444b2882e82da97256ef44a89f1dd4847f
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-09-28 02:43:35 +0700

    update

--- resources/views/laporancheck.blade.php
+++ resources/views/laporancheck.blade.php
@@ -1,16 +1,16 @@
  <table class="table table-striped">
      @foreach ($data as $d)     
-      <thead>       
-        <tr>               
-            <th colspan="3" style="text-align: center;">{{$d->peserta->NAMA_DEPAN}} {{$d->peserta->NAMA_BELAKANG}}</th>     
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($d->check as $c)
-          <tr>
-             {{$c->TGL_CHECK}}          
-          </tr>
-        @endforeach
-      </tbody>
+        <thead>       
+            <tr>               
+                <th colspan="3" style="text-align: center;">{{$d->peserta->NAMA_DEPAN}} {{$d->peserta->NAMA_BELAKANG}}</th>     
+            </tr>
+        </thead>
+        <tbody>          
+            @foreach ($d->check as $c)
+            <tr>
+                <td> {{$c->TGL_CHECK}} </td>                
+            </tr>
+            @endforeach
+        </tbody>
        @endforeach     
 </table>
