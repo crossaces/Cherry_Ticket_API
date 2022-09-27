28c93fda41da2029641cb37183309deaf681a196
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-09-28 02:29:48 +0700

    update

--- resources/views/laporancheck.blade.php
+++ resources/views/laporancheck.blade.php
@@ -2,7 +2,7 @@
       <thead>       
         <tr>
             @foreach ($data as $d)         
-              <th colspan="3">{{$d->peserta->NAMA_DEPAN}} {{$d->peserta->NAMA_BELAKANG}}</th>     
+              <th colspan="3" style="text-align: center;">{{$d->peserta->NAMA_DEPAN}} {{$d->peserta->NAMA_BELAKANG}}</th>     
             @endforeach     
         </tr>
       </thead>
