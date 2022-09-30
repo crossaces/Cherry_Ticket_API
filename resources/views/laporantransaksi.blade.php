d410d9e7fd1d133692c4cc7e74a5a3d8fbc55faa
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2022-10-01 05:45:37 +0700

    update

--- resources/views/laporantransaksi.blade.php
+++ resources/views/laporantransaksi.blade.php
@@ -54,3 +54,60 @@
         @endforeach     
         </tbody>       
 </table>
+
+<table class="table table-striped">    
+        <thead>       
+            <tr>               
+                <th  style="text-align: center;">Full Name</th>  
+                <th  style="text-align: center;">Transaction Date</th>                
+                <th  style="text-align: center;">Method Payment</th>  
+                <th  style="text-align: center;">Status</th>
+                <th  style="text-align: center;">Total Price</th>  
+                <th  colspan="3" style="text-align: center;">Order</th>                        
+            </tr>
+        </thead>
+        <tbody>     
+         @foreach ($data as $d)   
+            <tr>
+                <td  rowspan="{{count($d->order)+1}}" style="vertical-align : center;text-align:center;" >
+                    {{$d->peserta->NAMA_DEPAN}} {{$d->peserta->NAMA_BELAKANG}}
+                </td>  
+                <td  rowspan="{{count($d->order)+1}}" style="vertical-align : center;text-align:center;" >
+                   {{$d->TGL_TRANSAKSI}}
+                </td>   
+                <td  rowspan="{{count($d->order)+1}}" style="vertical-align : center;text-align:center;" >
+                   {{$d->METODE_PEMBAYARAN}}
+                </td>    
+                 <td  rowspan="{{count($d->order)+1}}" style="vertical-align : center;text-align:center;" >
+                   {{$d->STATUS_TRANSAKSI}}
+                </td> 
+                <td  rowspan="{{count($d->order)+1}}" style="vertical-align : center;text-align:center;" >
+                   {{$d->TOTAL_HARGA}}
+                </td>               
+                <td style="vertical-align : left;text-align:left;" >
+                   Tiket Type
+                </td> 
+                 <td style="vertical-align : left;text-align:left;" >
+                    <b>Quantity</b>                   
+                </td>        
+                <td style="vertical-align : left;text-align:left;" >
+                    <b>Subtotal</b>                          
+                </td>                   
+            </tr>                                                                                                         
+            @foreach ($d->order as $o)    
+             <tr>                                         
+                <td style="vertical-align : left;text-align:left;" >
+                    {{$o->tiket->NAMA_TIKET}}
+                </td> 
+                 <td style="vertical-align : left;text-align:left;" >
+                    {{$o->JUMLAH}}
+                </td> 
+                 <td style="vertical-align : left;text-align:left;" >
+                    {{$o->SUBTOTAL}}
+                </td> 
+             </tr>                  
+            @endforeach      
+                  
+        @endforeach     
+        </tbody>       
+</table>
